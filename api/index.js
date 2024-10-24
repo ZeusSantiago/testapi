45e4d1b1cfc9ebdb3780afda3b8e3c5e667af71d
ZeusSantiago <163061179+ZeusSantiago@users.noreply.github.com>
2024-10-24 12:38:34 +0800

    trial

--- api/index.js
+++ api/index.js
@@ -1,3 +1,22 @@
-import app from "../server";
+import express from "express";
+import path from "path";
+import posts from "../routes/posts.js";
+import logger from "../routes/middleware/logger.js";
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Logger Middleware
+app.use(logger);
+
+//Routes
+app.use("/api/posts", posts);
+
+app.listen(port, () => console.log(`Server is running in ${port}`));
 
 export default app;
